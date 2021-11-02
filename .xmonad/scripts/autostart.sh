3357d3fb808d4c23e8ed1e7f878f61f310c4c5d4
Aleksander Kus <alex.qs234@gmail.com>
2021-11-02 09:34:36 +0100

    update trayer

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -7,7 +7,8 @@ setxkbmap -option caps:escape
 nm-applet &
 volumeicon &
 redshift-gtk -l 52.4:16.9 &
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype request --height 22 --transparent true --alpha 0 --tint 0x282c34 --padding 6 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype request --height 22 --transparent true --alpha 0 --tint 0x282c34 --padding 6 --monitor 0 &
+#trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &
 autokey-gtk &
 pavucontrol &
 dunst &
