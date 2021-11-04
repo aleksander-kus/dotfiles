312166243ce0251e8db065fa2c844232cf1204e0
Aleksander Kuś <alex.qs234@gmail.com>
2021-11-04 22:44:18 +0100

    add group field in ll exa alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -273,7 +273,7 @@ alias doompurge "~/.emacs.d/bin/doom purge"
 # Changing "ls" to "exa"
 alias ls 'exa --color=always --group-directories-first' # my preferred listing
 alias la 'exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll 'exa -al --color=always --group-directories-first'  # long format
+alias ll 'exa -alg --color=always --group-directories-first'  # long format
 alias lt 'exa -aT --color=always --group-directories-first' # tree listing
 
 # pacman and yay
