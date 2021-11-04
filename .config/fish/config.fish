cf25259cce1bc44d6bc5684ffdad940c7682e5c2
Aleksander Kuś <alex.qs234@gmail.com>
2021-11-04 22:44:03 +0100

    fix ssh-agent in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -240,8 +240,7 @@ end
 ### END OF FUNCTIONS ###
 
 ### SSH AGENT ###
-pgrep ssh-agent 1>/dev/null 2>&1
-if test ! $status -eq 0
+if test -z (pgrep ssh-agent)
   eval (ssh-agent -c)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
