d19afc2a7701b7d12fb8c5754507783707d6db45
Aleksander Kuś <alex.qs234@gmail.com>
2021-12-08 13:03:27 +0100

    add extract function to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -170,6 +170,53 @@ function remove
 
     rm $original_args
 end
+
+function extract --description "Expand or extract bundled & compressed files"
+    # no arguments, write usage
+    if test (count $argv) -eq 0
+        echo "Usage: extract [-option] [file ...] " >&2
+        exit 1
+    end
+    for i in $argv[1..-1]
+        if test ! -f $i
+            echo "extract: '$i' is not a valid file" >&2
+            continue
+        end
+        set success 0
+        set extension (string match -r ".*(\.[^\.]*)\$" $i)[2]
+        switch $extension
+            case '*.tar.gz' '*.tgz'
+                tar xv; or tar zxvf "$i"
+            case '*.tar.bz2' '*.tbz' '*.tbz2'
+                tar xvjf "$i"
+            case '*.tar.xz' '*.txz'
+                tar --xz -xvf "$i"; or xzcat "$i" | tar xvf -
+            case '*.tar.zma' '*.tlz'
+                tar --lzma -xvf "$i"; or lzcat "$i" | tar xvf -
+            case '*.tar'
+                tar xvf "$i"
+            case '*.gz'
+                gunzip -k "$i"
+            case '*.bz2'
+                bunzip2 "$i"
+            case '*.xz'
+                unxz "$i"
+            case '*.lzma'
+                unlzma "$i"
+            case '*.z'
+                uncompress "$i"
+            case '*.zip' '*.war' '*.jar' '*.sublime-package' '*.ipsw' '*.xpi' '*.apk' '*.aar' '*.whl'
+                clean-unzip "$i"
+            case '*.rar'
+                unrar x -ad "$i"
+            case '*.7z'
+                7za x "$i"
+            case '*'
+                echo "extract: '$i' cannot be extracted" >&2
+                set success 1
+        end
+    end
+end
 ### END OF FUNCTIONS ###
 
 ### SSH AGENT ###
