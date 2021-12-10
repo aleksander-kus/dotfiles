9282de5d8e37a5437552afa1369ae237204e6032
Aleksander Kuś <alex.qs234@gmail.com>
2021-12-10 12:24:14 +0100

    cleanup autostart.sh

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
-nitrogen --restore &
-picom &
-setxkbmap -option caps:escape
+## Desktop appearance
+nitrogen --restore &  # Wallpaper
+picom &               # Compositor
+
+## Trayer
 nm-applet &
 volumeicon &
-redshift-gtk -l 52.4:16.9 &
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype request --height 22 --transparent true --alpha 0 --tint 0x282c34 --padding 6 --monitor 0 &
-#trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &
-autokey-gtk &
-pavucontrol &
-dunst &
-udiskie --tray &
-lxsession &
+
+## Programs
+redshift-gtk &        # Blue light filter
+autokey-gtk &         # Text expanding
+pavucontrol &         # Volume control
+dunst &               # Notifications
+udiskie --tray &      # Automount
+lxsession &           # Session manager
+
 #DAY=$(date "+%u")
 #[ $DAY != "6" -a $DAY != "7" ] && teams &
-steam &
-# Poe
+# steam &
+
+## Poe
 #/home/alex/bin/Awakened-PoE-Trade-2.9.1.AppImage &
 #python /home/alex/dev/clienthelper.py &
+
+## Keyboard and cursor
+setxkbmap -option caps:escape
 xsetroot -cursor_name left_ptr
